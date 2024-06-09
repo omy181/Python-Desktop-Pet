4282943fa082e70b80b65dc8d6967b3568366b01
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2024-06-09 23:54:09 +0300

    animations fixed, support added

--- DesktopPet.py
+++ DesktopPet.py
@@ -5,6 +5,7 @@ from enum import Enum
 import time
 from win32api import GetSystemMetrics
 import sys
+from PIL import Image, ImageTk
 
 class State(Enum):
     IDLE = 0   
@@ -15,20 +16,18 @@ class State(Enum):
     WAKEUP = 5
     FALLING = 6
     CELEBRATION = 7
+    HUG = 8
+    HELP = 9
 
+floor_level = GetSystemMetrics(1)-270
 
 walk_speed = 6
-x = 1400
-y = 1000
+x = 0
+y = floor_level
 cycle = 0
 state_timer = 0
 state = State.IDLE
-idle_num =[1,2,3,4]
-sleep_num = [10,11,12,13,15]
-walk_left = [6,7]
-walk_right = [8,9]
 impath = 'Animations/'
-
 
 is_dragging = False
 is_clicked = False
@@ -44,7 +43,6 @@ def on_click(event):
     global is_clicked
     is_clicked = True    
 
-
 def on_unclick(event):
     global is_clicked,is_dragging,hold_timer
     
@@ -52,13 +50,12 @@ def on_unclick(event):
         if state == State.SLEEP:
             change_state(State.WAKEUP)
         elif state == State.IDLE:
-            change_state(state.LOVEING)
+            change_state(State.LOVEING)
     
     is_clicked = False
     hold_timer = 0
     is_dragging = False
 
-    
 def change_state(new_state):
     global state,state_timer,fall_speed,long_sleep
     
@@ -85,9 +82,6 @@ def move_toward_mouse(cursor_x):
     
     return dir    
 
-floor_level = GetSystemMetrics(1)-270
-
-
 def is_on_ground():
     global y,fall_speed
     return y+fall_speed >= floor_level    
@@ -101,8 +95,6 @@ def gravity():
     else:
         fall_speed = 0
         y = floor_level 
-        
-    
 
 def gif_work(cycle,frames):
     if cycle < len(frames) -1:
@@ -123,22 +115,37 @@ fall_speed = 0
 sleep_time = 0
 
 def update():
-    
     global state,cycle,x,y,state_timer,gravity_enabled,fall_speed,is_dragging,hold_timer,is_clicked,sleep_time
     
-    
-
     cursor_x, cursor_y = pyautogui.position()
     if is_clicked:
         hold_timer += 1
         if hold_timer > 1:
-            change_state(state.DRAGGING)   
+            change_state(State.DRAGGING)   
             is_dragging = True  
     else:
         hold_timer = 0
         is_dragging = False
     
     match state:
+        case State.HELP:
+            lcycle = gif_work(cycle,help)
+                        
+            if(lcycle == 0):
+                change_state(State.IDLE)
+            else:
+                update_frame(help)   
+                time.sleep(0.2)
+        
+        case State.HUG:
+            lcycle = gif_work(cycle,hug)
+                        
+            if(lcycle == 0):
+                change_state(State.IDLE)
+            else:
+                update_frame(hug)   
+                time.sleep(0.2)
+            
         case State.CELEBRATION:
             lcycle = gif_work(cycle,celebration)
                         
@@ -158,16 +165,16 @@ def update():
         case State.DRAGGING:
             if not is_dragging:
                 change_state(State.IDLE)
-
-            
+          
             x = cursor_x-100
             y = cursor_y-20
             time.sleep(0.06)
             update_frame(hold)  
+            
         case State.IDLE:
             state_timer += 1      
             
-            if(distance(cursor_x,x) > 1200):
+            if(not stay_still.get() and distance(cursor_x,x) > 700):
                 change_state(State.FOLLOW_CURSOR)
                 
             if gravity_enabled.get() and not is_on_ground():
@@ -177,8 +184,11 @@ def update():
             time.sleep(0.2)
             
             if(state_timer > 100):
+                
+                random_action =  random.choice([State.SLEEP,State.SLEEP,State.HUG,State.CELEBRATION,State.HELP,State.IDLE,State.IDLE,State.IDLE,State.IDLE])
+                   
                 sleep_time = random.randint(200,400)
-                change_state(State.SLEEP)
+                change_state(random_action)
             
         case State.SLEEP:
             state_timer += 1
@@ -211,10 +221,7 @@ def update():
                 if dir == -1:
                     update_frame(walk_positive)
                 else:
-                    update_frame(walk_negative)                                     
-                
-
-                
+                    update_frame(walk_negative)                                           
                 
         case State.LOVEING:
             
@@ -226,13 +233,9 @@ def update():
             if(cycle == 0):
                 change_state(State.IDLE)
             
-            
-            
-        
-
-    
+   
     window.after(5,update)
-    
+
 def start_celebration():
     change_state(State.CELEBRATION)
 
@@ -248,20 +251,39 @@ def set_long_sleep():
         change_state(State.SLEEP) 
     else:
         change_state(State.WAKEUP)
+        
+def set_hug():
+    change_state(State.HUG)
+    
+def set_help():
+    change_state(State.HELP)
+    
+def set_love():
+    change_state(State.LOVEING)
     
 window = tk.Tk()
 
-#call buddy's action gif
-idle = [tk.PhotoImage(file=impath+'Bunidle.gif',format = 'gif -index %i' %(i)) for i in range(2)]
-hold = [tk.PhotoImage(file=impath+'BunHold.gif',format = 'gif -index %i' %(i)) for i in range(4)]
-sleep = [tk.PhotoImage(file=impath+'BunSleep.gif',format = 'gif -index %i' %(i)) for i in range(4)]
-sleep_to_idle = [tk.PhotoImage(file=impath+'BunWake.gif',format = 'gif -index %i' %(i)) for i in range(4)]
-walk_positive = [tk.PhotoImage(file=impath+'BunWalkLeft.gif',format = 'gif -index %i' %(i)) for i in range(5)]
-walk_negative = [tk.PhotoImage(file=impath+'BunWalkRight.gif',format = 'gif -index %i' %(i)) for i in range(5)]
-love = [tk.PhotoImage(file=impath+'BunLove.gif',format = 'gif -index %i' %(i)) for i in range(9)]
-celebration = [tk.PhotoImage(file=impath+'BunCelebration.gif',format = 'gif -index %i' %(i)) for i in range(25)]
+# Helper function to load images with transparency handled
+def load_images(file_path, frames):
+    images = []
+    for i in range(frames):
+        img = Image.open(file_path)
+        img.seek(i)
+        photo = ImageTk.PhotoImage(img.convert("RGBA"))
+        images.append(photo)
+    return images
 
-
+# Load animations with transparency handled
+idle = load_images(impath+'Bunidle.gif', 2)
+hold = load_images(impath+'BunHold.gif', 4)
+sleep = load_images(impath+'BunSleep.gif', 4)
+sleep_to_idle = load_images(impath+'BunWake.gif', 4)
+walk_positive = load_images(impath+'BunWalkLeft.gif', 5)
+walk_negative = load_images(impath+'BunWalkRight.gif', 5)
+love = load_images(impath+'BunLove.gif', 9)
+celebration = load_images(impath+'BunCelebration.gif', 25)
+hug = load_images(impath+'BunHug.gif', 15)
+help = load_images(impath+'BunHelp.gif', 16)
 
 #window configuration
 window.config(highlightbackground='black')
@@ -277,18 +299,23 @@ label.pack()
 gravity_enabled = tk.BooleanVar()
 always_on_top = tk.BooleanVar(value= True)
 long_sleep = tk.BooleanVar()
+stay_still = tk.BooleanVar()
 
 set_always_on_top()
 
 r_menu = tk.Menu(window, tearoff = 0) 
+r_menu.add_command(label ="Love", command = set_love)
+r_menu.add_command(label ="Hug", command = set_hug)
+r_menu.add_command(label ="Surprise", command = start_celebration)
+r_menu.add_command(label ="Support", command = set_help)
+r_menu.add_checkbutton(label ="Winter sleep",onvalue=True,offvalue=False,variable=long_sleep,command=set_long_sleep)
+r_menu.add_separator()
 r_menu.add_checkbutton(label ="Gravity",onvalue=True,offvalue=False,variable=gravity_enabled)
 r_menu.add_checkbutton(label ="Always on top",onvalue=True,offvalue=False,variable=always_on_top,command=set_always_on_top)
+r_menu.add_checkbutton(label ="Stay still",onvalue=True,offvalue=False,variable=stay_still)
 r_menu.add_command(label ="Set floor here", command = set_floor_here)
-r_menu.add_checkbutton(label ="Winter sleep",onvalue=True,offvalue=False,variable=long_sleep,command=set_long_sleep)
-r_menu.add_command(label ="Surprise", command = start_celebration)
 r_menu.add_separator()
 r_menu.add_command(label ="Bye Bye", command = sys.exit)
-
 
 #loop the program
 window.after(1,update)
